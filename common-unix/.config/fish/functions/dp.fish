460dfbd14f06fad6ba426399d5748b6660b9772c
Binwalker <gensky377@gmail.com>
2024-06-30 23:57:22 +0900

    fix: improve error message

--- common-unix/.config/fish/functions/dp.fish
+++ common-unix/.config/fish/functions/dp.fish
@@ -1,12 +1,12 @@
 function dp --description "List containers"
     if not command -q docker; or command -v docker | string match -qr "^/mnt"
-        echo "$(set_color red)Error:$(set_color normal) `docker` command not found" >&2
+        echo "$(set_color red)Error$(set_color normal): `docker` command not found" >&2
         return 1
     end
 
     set error_msg (command sudo docker version 2>&1 1>/dev/null)
     test (string length -- "$error_msg") -gt 0
-    and echo "$(set_color red)Error:$(set_color normal) $error_msg" >&2
+    and echo "$(set_color red)Error$(set_color normal): $error_msg" >&2
     and return 1
 
     sudo docker ps --format "{{.Names}} {{.Status}}" $argv | while read -l container_name container_status
