18e8c8e17b95a1784996bcfbecc7594ee3abd139
Binwalker <gensky377@gmail.com>
2024-06-30 21:48:57 +0900

    fix: add error handling for docker command in dp function

--- common-unix/.config/fish/functions/dp.fish
+++ common-unix/.config/fish/functions/dp.fish
@@ -1,4 +1,14 @@
 function dp --description "List containers"
+    if not command -q docker; or command -v docker | string match -qr "^/mnt"
+        echo "$(set_color red)Error:$(set_color normal) `docker` command not found" >&2
+        return 1
+    end
+
+    set error_msg (command sudo docker version 2>&1 1>/dev/null)
+    test (string length -- "$error_msg") -gt 0
+    and echo "$(set_color red)Error:$(set_color normal) $error_msg" >&2
+    and return 1
+
     sudo docker ps --format "{{.Names}} {{.Status}}" $argv | while read -l container_name container_status
         if string match -qr "Exited*" $container_status
             set exited_containers $exited_containers "$container_name (Exited)"
@@ -21,7 +31,7 @@ function dp --description "List containers"
     else
         set msg "Running: $(count $running_containers)"
     end
-    set msg_length (string length -- $msg)
+    set msg_length (string length -- "$msg")
     test $total -gt 0; and set seps (string repeat -n $msg_length "-"); and echo $seps >&2
-    echo $msg >&2
+    echo "$msg" >&2
 end
