4472c6bc9c43823e41ad3760c3e6b06b4e117afb
Binwalker <gensky377@gmail.com>
2024-07-03 21:43:39 +0900

    refactor: use exact match with fzf in docker functions

--- common-unix/.config/fish/functions/docker/dl.fish
+++ common-unix/.config/fish/functions/docker/dl.fish
@@ -1,7 +1,7 @@
 function dl --description "Logs of container"
     _docker_check; or return 1
-    set container_name (_container_list -a | fzf --exact | awk '{print $1}')
+    set container_name (_container_list -a | fzf --exact --query="$argv[1]" -1 | awk '{print $1}')
     if string length -- $container_name &>/dev/null
-        docker logs $container_name $argv
+        docker logs -f $container_name
     end
 end
--- common-unix/.config/fish/functions/docker/drm.fish
+++ common-unix/.config/fish/functions/docker/drm.fish
@@ -1,6 +1,6 @@
 function drm --description "Remove container"
     _docker_check; or return 1
-    set container_name (_container_list -a | fzf --exact | awk '{print $1}')
+    set container_name (_container_list -a | fzf --exact --query="$argv[1]" -1 | awk '{print $1}')
     if string length -- $container_name &>/dev/null
         docker rm -f $container_name
     end
