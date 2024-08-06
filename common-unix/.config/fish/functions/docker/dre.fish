0ea405804e2440524c45a7683f9b4b314a73840d
Binwalker <gensky377@gmail.com>
2024-08-06 22:09:23 +0900

    feat(fish function): multi select for dre, drm

--- common-unix/.config/fish/functions/docker/dre.fish
+++ common-unix/.config/fish/functions/docker/dre.fish
@@ -1,6 +1,8 @@
 function dre --description "Restart container"
     _docker_check; or return 1
-    set container_name (_container_list -a | fzf --exact --query="$argv[1]" -1 | awk '{print $1}')
+    set container_name (_container_list -a \
+    | fzf --exact --query="$argv[1]" -1 --multi --bind 'ctrl-a:select-all,ctrl-d:deselect-all,tab:toggle' \
+    | awk '{print $1}')
     if string length -- $container_name &>/dev/null
         docker restart $container_name
     end
--- common-unix/.config/fish/functions/docker/drm.fish
+++ common-unix/.config/fish/functions/docker/drm.fish
@@ -1,6 +1,8 @@
 function drm --description "Remove container"
     _docker_check; or return 1
-    set container_name (_container_list -a | fzf --exact --query="$argv[1]" -1 | awk '{print $1}')
+    set container_name (_container_list -a \
+    | fzf --exact --query="$argv[1]" -1 --multi --bind 'ctrl-a:select-all,ctrl-d:deselect-all,tab:toggle' \
+    | awk '{print $1}')
     if string length -- $container_name &>/dev/null
         docker rm -f $container_name
     end
