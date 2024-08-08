d9fa4ef95473f80c540588978d2f139a2c84a1b4
Binwalker <gensky377@gmail.com>
2024-08-08 23:07:32 +0900

    refactor: docker logs only since current time

--- common-unix/.config/fish/functions/docker/dl.fish
+++ common-unix/.config/fish/functions/docker/dl.fish
@@ -2,6 +2,6 @@ function dl --description "Logs of container"
     _docker_check; or return 1
     set container_name (_container_list -a | fzf --exact --query="$argv[1]" -1 | awk '{print $1}')
     if string length -- $container_name &>/dev/null
-        docker logs -f $container_name
+        docker logs -f --since "$(date +%Y-%m-%dT%H:%M:%S)" $container_name
     end
 end
