4bb3c150578be1f5d5e4b6a931ae9d46e953c633
Binwalker <gensky377@gmail.com>
2024-08-10 06:01:17 +0900

    refactor: da with preview window

--- common-unix/.config/fish/functions/docker/da.fish
+++ common-unix/.config/fish/functions/docker/da.fish
@@ -2,8 +2,15 @@
 
 function da --description "Attach to container"
     _docker_check; or return 1
-    set container_name (_container_list | fzf --query "$argv[1]" | awk '{print $1}')
-    if string length -- $container_name &>/dev/null
-        docker attach $container_name
-    end
+    _container_list | fzf --with-nth -1 --query "$argv[1]" \
+        --preview "docker ps -a --filter id={1} \
+            --format \
+'ID:      {{.ID}}
+Image:   {{.Image}}
+Command: {{.Command}}
+Created: {{.CreatedAt}}
+Status:  {{.Status}}
+Ports:   {{.Ports}}
+Name:    {{.Names}}'" \
+        --bind "enter:become(docker attach {1})"
 end
