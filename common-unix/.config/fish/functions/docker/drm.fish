9a1cc1578d361dc6c486be9da303b88b8325c427
lin-bozhou <lin.bozhou@donuts.ne.jp>
2024-08-09 18:32:46 +0900

    refactor: update FZF_DEFAULT_OPTS in fish config for better preview window

--- common-unix/.config/fish/functions/docker/drm.fish
+++ common-unix/.config/fish/functions/docker/drm.fish
@@ -1,7 +1,7 @@
 function drm --description "Remove container"
     _docker_check; or return 1
     set container_name (_container_list -a \
-        | fzf --ansi --exact --query="$argv[1]" -1 --multi --bind 'ctrl-a:select-all,ctrl-d:deselect-all,tab:toggle' \
+        | fzf --query="$argv[1]" -1 --multi --bind 'ctrl-a:select-all,ctrl-d:deselect-all,tab:toggle' \
         | awk '{print $1}')
     if string length -- $container_name &>/dev/null
         docker rm -f $container_name
--- debian/.config/fish/config.fish
+++ debian/.config/fish/config.fish
@@ -3,8 +3,16 @@ status is-interactive; or return
 zoxide init fish | source
 
 set -gx EDITOR nvim
-set -U fish_greeting
-set fish_function_path $HOME/.config/fish/functions/*/ $fish_function_path
+set -gx fish_greeting
+set -gx fish_function_path $HOME/.config/fish/functions/*/ $fish_function_path
+set -gx FZF_DEFAULT_OPTS "--ansi --height=40% --layout=reverse --info=inline --border \
+    --preview '
+        if test -f {}
+            batcat --color=always {}
+        else
+            echo -e {}
+        end'
+    --preview-window=border-none"
 
 alias cls=clear
 alias cat=batcat
