f8435e11b392d8b49fa76b077e65ed9c34ac051a
Binwalker <gensky377@gmail.com>
2024-08-10 06:00:40 +0900

    refactor: include container ID in container list

--- common-unix/.config/fish/functions/docker/_container_list.fish
+++ common-unix/.config/fish/functions/docker/_container_list.fish
@@ -3,11 +3,11 @@
 function _container_list --description "List containers"
     _docker_check; or return 1
 
-    docker ps --format "{{.Names}} {{.Status}}" $argv | while read -l container_name container_status
+    docker ps --format "{{.ID}} {{.Names}} {{.Status}}" $argv | while read -l container_id container_name container_status
         if string match -qr "Exited*" $container_status
-            set exited_containers $exited_containers "$container_name (Exited)"
+            set exited_containers $exited_containers "$container_id $container_name (Exited)"
         else if string match -qr "Up*" $container_status
-            set running_containers $running_containers $container_name
+            set running_containers $running_containers "$container_id $container_name"
         end
     end
 
