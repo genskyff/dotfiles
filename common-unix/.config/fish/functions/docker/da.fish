cd8aada696267ff8fadd62d59984e5f5c7abf924
lin-bozhou <lin.bozhou@donuts.ne.jp>
2024-08-09 15:54:33 +0900

    feat: use ANSI escape codes for colored output in docker functions

--- common-unix/.config/fish/functions/docker/da.fish
+++ common-unix/.config/fish/functions/docker/da.fish
@@ -1,6 +1,6 @@
 function da --description "Attach to container"
     _docker_check; or return 1
-    set container_name (_container_list | fzf --exact --query="$argv[1]" -1 | awk '{print $1}')
+    set container_name (_container_list | fzf --ansi --exact --query="$argv[1]" -1 | awk '{print $1}')
     if string length -- $container_name &>/dev/null
         docker attach $container_name
     end
--- common-unix/.config/fish/functions/docker/dl.fish
+++ common-unix/.config/fish/functions/docker/dl.fish
@@ -1,6 +1,6 @@
 function dl --description "Logs of container"
     _docker_check; or return 1
-    set container_name (_container_list -a | fzf --exact --query="$argv[1]" -1 | awk '{print $1}')
+    set container_name (_container_list -a | fzf --ansi --exact --query="$argv[1]" -1 | awk '{print $1}')
     if string length -- $container_name &>/dev/null
         docker logs -f --since "$(date +%Y-%m-%dT%H:%M:%S)" $container_name
     end
