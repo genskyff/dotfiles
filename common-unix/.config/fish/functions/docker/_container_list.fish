98e954cc60e52fba956a7f2cdcc0a763ea1d2286
Binwalker <gensky377@gmail.com>
2024-08-10 07:07:57 +0900

    refactor: dp

--- common-unix/.config/fish/functions/docker/_container_list.fish
+++ common-unix/.config/fish/functions/docker/_container_list.fish
@@ -5,17 +5,14 @@ function _container_list --description "List containers"
 
     docker ps --format "{{.ID}} {{.Names}} {{.Status}}" $argv | while read -l container_id container_name container_status
         if string match -qr "Exited*" $container_status
-            set exited_containers $exited_containers "$container_id $container_name (Exited)"
+            set exited_containers $exited_containers "$(set_color red)$container_id $container_name (Exited)$(set_color normal)"
         else if string match -qr "Up*" $container_status
-            set running_containers $running_containers "$container_id $container_name"
+            set running_containers $running_containers "$(set_color green)$container_id $container_name$(set_color normal)"
         end
     end
 
-    for container in $exited_containers
-        echo -e "$(set_color red)$container$(set_color normal)"
-    end
-
-    for container in $running_containers
-        echo -e "$(set_color green)$container$(set_color normal)"
+    set all_containers $exited_containers $running_containers
+    for container in $all_containers
+        echo -e $container
     end
 end
--- common-unix/.config/fish/functions/docker/dp.fish
+++ common-unix/.config/fish/functions/docker/dp.fish
@@ -3,14 +3,7 @@
 function dp --description "List containers with status"
     _docker_check; or return 1
 
-    set result (docker ps --format "{{.ID}} {{.Names}} {{.Status}}" -a $argv)
-    if test $status -ne 0
-        echo -n "$result"
-        return 1
-    end
-
-    for container in $result
-        echo $container | read -l container_id container_name container_status
+    docker ps -a --format "{{.ID}} {{.Names}} {{.Status}}" | while read -l container_id container_name container_status
         if string match -qr "Exited*" $container_status
             set exited_containers $exited_containers "$(set_color red)$container_id $container_name (Exited)$(set_color normal)"
         else if string match -qr "Up*" $container_status
@@ -18,14 +11,14 @@ function dp --description "List containers with status"
         end
     end
 
-    set all_containers $running_containers $exited_containers
+    set all_containers $exited_containers $running_containers
     set total (count $all_containers)
     set message_ori "Total: $total Running: $(count $running_containers) Exited: $(count $exited_containers)"
     set message "$(set_color blue)Total: $total $(set_color green)Running: $(count $running_containers) $(set_color red)Exited: $(count $exited_containers)$(set_color normal)"
     set message_length (string length -- "$message_ori")
 
     string join \n $all_containers \
-        | fzf  --with-nth -1 \
+        | fzf --with-nth "2.." \
             --preview "fish $HOME/.config/fish/functions/docker/fzf_preview.fish {1}" \
             --header "$message"
 end
