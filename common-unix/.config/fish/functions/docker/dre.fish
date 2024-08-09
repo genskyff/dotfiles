57f5317fa43ca0874ad7c4e7bf1ae82a50246a66
Binwalker <gensky377@gmail.com>
2024-08-10 07:27:21 +0900

    fix: multi select in dre, drm

--- common-unix/.config/fish/functions/docker/dre.fish
+++ common-unix/.config/fish/functions/docker/dre.fish
@@ -5,5 +5,5 @@ function dre --description "Restart container"
     _container_list -a \
         | fzf --with-nth "2.." --query "$argv" --multi \
             --preview "fish $HOME/.config/fish/functions/docker/fzf_preview.fish {1}" \
-            --bind "ctrl-a:select-all,ctrl-d:deselect-all,tab:toggle,enter:become(docker restart {2})"
+            --bind "ctrl-a:select-all,ctrl-d:deselect-all,tab:toggle,enter:become(docker restart {+2})"
 end
