927ffb9d99ab9ed475eb374b615c83f8cf632e6e
Binwalker <gensky377@gmail.com>
2024-08-10 06:06:22 +0900

    refactor: update da function to use fzf_preview.fish for container details preview

--- common-unix/.config/fish/functions/docker/da.fish
+++ common-unix/.config/fish/functions/docker/da.fish
@@ -3,14 +3,6 @@
 function da --description "Attach to container"
     _docker_check; or return 1
     _container_list | fzf --with-nth -1 --query "$argv[1]" \
-        --preview "docker ps -a --filter id={1} \
-            --format \
-'ID:      {{.ID}}
-Image:   {{.Image}}
-Command: {{.Command}}
-Created: {{.CreatedAt}}
-Status:  {{.Status}}
-Ports:   {{.Ports}}
-Name:    {{.Names}}'" \
+        --preview 'fish $HOME/.config/fish/functions/docker/fzf_preview.fish {1}' \
         --bind "enter:become(docker attach {1})"
 end
