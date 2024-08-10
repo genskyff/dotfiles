1a68f1a4731bfdbfc941b3e2ee71c1b9a4981898
Binwalker <gensky377@gmail.com>
2024-08-11 03:04:47 +0900

    refactor: update gb function to use git switch instead of git checkout

--- common-unix/.config/fish/functions/git/gb.fish
+++ common-unix/.config/fish/functions/git/gb.fish
@@ -14,5 +14,5 @@ function gb --description "git branch with fzf"
     string join \n $branches \
         | fzf --preview "git log {-1} --oneline --graph --date="format:%y/%m/%d" --color=always \
                 --format='%C(auto)%cd %h%d <%<(6,trunc)%an> %s'" \
-            --bind "enter:become(git checkout {-1})" $header
+            --bind "enter:become(git switch {-1})" $header
 end
