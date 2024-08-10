c2f91b8bde0bda9d42d9c1be25ad328bdf263113
Binwalker <gensky377@gmail.com>
2024-08-10 21:02:04 +0900

    refactor: Update fish_edit and fish_function_edit functions to use $EDITOR

--- common-unix/.config/fish/functions/fish_edit.fish
+++ common-unix/.config/fish/functions/fish_edit.fish
@@ -3,10 +3,9 @@
 function fish_edit --description "Edit fish configuration"
     set config_path $HOME/.config/fish
     set files $config_path/config.fish $config_path/conf.d/*.fish
-    set nth (math (string split "/" -- $config_path | count) + 1)
-    set selected (string join \n $files | fzf --with-nth "$nth.." --delimiter "/" --query "$argv[1]")
-
-    if string length -- $selected &>/dev/null
-        nvim $selected
-    end
+    set nth (math (string split / -- $config_path | count) + 1)
+    test -n "$EDITOR"; or set EDITOR nvim
+    string join \n $files \
+        | fzf --with-nth "$nth.." --delimiter / --query "$argv" \
+            --bind "enter:become($EDITOR {})"
 end
--- common-unix/.config/fish/functions/fish_function_edit.fish
+++ common-unix/.config/fish/functions/fish_function_edit.fish
@@ -3,10 +3,9 @@
 function fish_function_edit --description "Edit fish function"
     set function_path $HOME/.config/fish/functions
     set files $function_path/**/*.fish
-    set nth (math (string split "/" -- $function_path | count) + 1)
-    set selected (string join \n $files | fzf --with-nth "$nth.." --delimiter "/" --query "$argv[1]")
-
-    if string length -- $selected &>/dev/null
-        nvim $selected
-    end
+    set nth (math (string split / -- $function_path | count) + 1)
+    test -n "$EDITOR"; or set EDITOR nvim
+    string join \n $files \
+        | fzf --with-nth "$nth.." --delimiter / --query "$argv" \
+            --bind "enter:become($EDITOR {})"
 end
