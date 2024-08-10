202be6bba695a98cb950eb996f0841121549ef15
Binwalker <gensky377@gmail.com>
2024-08-10 21:01:45 +0900

    refactor: change delimiter in fzf preview

--- common-unix/.config/fish/functions/docker/fzf_preview.fish
+++ common-unix/.config/fish/functions/docker/fzf_preview.fish
@@ -1,9 +1,9 @@
 #!/usr/bin/env fish
 
 docker ps -a --filter id=$argv \
-    --format '{{.ID}}|{{.Image}}|{{.Command}}|{{.CreatedAt}}|{{.Status}}|{{.Ports}}|{{.Names}}' \
+    --format "{{.ID}}/{{.Image}}/{{.Command}}/{{.CreatedAt}}/{{.Status}}/{{.Ports}}/{{.Names}}" \
     | while read -l line
-        set -l fields (string split '|' $line)
+        set -l fields (string split / $line)
         echo (set_color blue)"ID      "(set_color normal)$fields[1]
         echo (set_color green)"Image   "(set_color normal)$fields[2]
         echo (set_color yellow)"Command "(set_color normal)$fields[3]
