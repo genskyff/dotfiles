4a0f086d0c32a20e1c73978f6b8f7b4da9e7ab67
Binwalker <gensky377@gmail.com>
2024-08-10 17:00:36 +0900

    refactor: improve docker ps output formatting

--- common-unix/.config/fish/functions/docker/fzf_preview.fish
+++ common-unix/.config/fish/functions/docker/fzf_preview.fish
@@ -1,11 +1,15 @@
 #!/usr/bin/env fish
 
 docker ps -a --filter id=$argv \
-    --format \
-'ID:      {{.ID}}
-Image:   {{.Image}}
-Command: {{.Command}}
-Created: {{.CreatedAt}}
-Status:  {{.Status}}
-Ports:   {{.Ports}}
-Name:    {{.Names}}'
+    --format '{{.ID}}|{{.Image}}|{{.Command}}|{{.CreatedAt}}|{{.Status}}|{{.Ports}}|{{.Names}}' \
+    | while read -l line
+        set -l fields (string split '|' $line)
+        echo (set_color blue)"ID      "(set_color normal)$fields[1]
+        echo (set_color green)"Image   "(set_color normal)$fields[2]
+        echo (set_color yellow)"Command "(set_color normal)$fields[3]
+        echo (set_color cyan)"Created "(set_color normal)$fields[4]
+        echo (set_color yellow)"Status  "(set_color normal)$fields[5]
+        echo (set_color green)"Ports   "(set_color normal)$fields[6]
+        echo (set_color blue)"Name    "(set_color normal)$fields[7]
+        echo ""
+    end
