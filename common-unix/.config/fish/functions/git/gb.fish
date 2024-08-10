960819cbb79cbb16f70494a31603ce47e37401cd
Binwalker <gensky377@gmail.com>
2024-08-10 19:27:02 +0900

    fix: preview error when detached in gb

--- common-unix/.config/fish/functions/git/gb.fish
+++ common-unix/.config/fish/functions/git/gb.fish
@@ -2,8 +2,15 @@
 
 function gb --description "git branch with fzf"
     _git_check; or return 1
-    git branch \
+
+    set branches (git branch)
+    if not test (git branch --show-current)
+        set header --header $branches[1]
+        set branches $branches[2..-1]
+    end
+
+    string join \n $branches \
         | fzf --preview "git log {-1} --oneline --graph --date=short --color=always \
                 --format='%C(auto)%cd %h%d <%<(6,trunc)%an> %s'" \
-            --bind "enter:become(git checkout {-1})"
+            --bind "enter:become(git checkout {-1})" $header
 end
