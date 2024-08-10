90bced8ef6ce289a85a99c051470127dcbfc02b1
Binwalker <gensky377@gmail.com>
2024-08-10 20:25:31 +0900

    fix: fish_function_path not work

--- common-unix/.config/fish/config.fish
+++ common-unix/.config/fish/config.fish
@@ -7,8 +7,8 @@ starship init fish | source
 zoxide init fish | source
 
 set -gx EDITOR nvim
-set -gx fish_greeting
-set -gx fish_function_path $HOME/.config/fish/functions/*/ $fish_function_path
+set -U fish_greeting
+set fish_function_path $HOME/.config/fish/functions/*/ $fish_function_path
 set -gx FZF_DEFAULT_OPTS "--ansi --height 50% --layout reverse --info inline --border \
     --preview 'fish $HOME/.config/fish/functions/fzf_preview.fish {}' \
     --preview-window 'border-left,60%' \
