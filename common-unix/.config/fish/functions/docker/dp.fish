f651e78e7892ecfcbbae743db200a59f69a9ab3e
Binwalker <gensky377@gmail.com>
2024-08-10 16:43:57 +0900

    refactor:  dp output

--- common-unix/.config/fish/functions/docker/dp.fish
+++ common-unix/.config/fish/functions/docker/dp.fish
@@ -3,19 +3,12 @@
 function dp --description "List containers with status"
     _docker_check; or return 1
 
-    docker ps -a --format "{{.ID}} {{.Names}} {{.Status}}" | while read -l container_id container_name container_status
-        if string match -qr "Exited*" $container_status
-            set exited_containers $exited_containers "$(set_color red)$container_id $container_name (Exited)$(set_color normal)"
-        else if string match -qr "Up*" $container_status
-            set running_containers $running_containers "$(set_color green)$container_id $container_name$(set_color normal)"
-        end
-    end
+    set all_containers (_container_list -a)
+    set total_count (count $all_containers)
+    set exited_count (string match -r "Exited" $all_containers | count)
+    set running_count (math $total_count - $exited_count)
 
-    set all_containers $exited_containers $running_containers
-    set total (count $all_containers)
-    set message_ori "Total: $total Running: $(count $running_containers) Exited: $(count $exited_containers)"
-    set message "$(set_color blue)Total: $total $(set_color green)Running: $(count $running_containers) $(set_color red)Exited: $(count $exited_containers)$(set_color normal)"
-    set message_length (string length -- "$message_ori")
+    set message "$(set_color blue)Total: $total_count $(set_color green)Running: $running_count $(set_color red)Exited: $exited_count$(set_color normal)"
 
     string join \n $all_containers \
         | fzf --with-nth "2.." \
