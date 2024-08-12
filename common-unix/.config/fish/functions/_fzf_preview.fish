f66d3f9233f991a6486bce7d82559fa576adc0d8
Binwalker <gensky377@gmail.com>
2024-08-12 20:43:24 +0900

    feat: update _fzf_preview.fish to use lsd with color and icon options

--- common-unix/.config/fish/functions/_fzf_preview.fish
+++ common-unix/.config/fish/functions/_fzf_preview.fish
@@ -3,13 +3,13 @@
 if test -f $argv
     if file -b $argv | grep -qE "(text|empty)"
         command -qv bat
-        and bat --color=always $argv
-        or batcat --color=always $argv
+        and bat --color always $argv
+        or batcat --color always $argv
     else
         file $argv
     end
 else if test -d $argv
-    lsd --tree --depth 1 $argv
+    lsd --color always --tree --depth 1 $argv
 else
     echo -e $argv
 end
