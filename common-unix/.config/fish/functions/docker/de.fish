f258cd3eef793397d26b9b09d6252578353bf092
Binwalker <gensky377@gmail.com>
2024-08-15 21:47:20 +0900

    fix: de on macos

--- common-unix/.config/fish/functions/docker/de.fish
+++ common-unix/.config/fish/functions/docker/de.fish
@@ -1,10 +1,9 @@
 #!/usr/bin/env fish
 
 function de --description "Execute command in container"
-_docker_check
-or return 1
-_container_list | fzf --with-nth 2 \
-    --preview "fish $HOME/.config/fish/functions/docker/_fzf_preview.fish {1}" \
-    --bind "start:toggle-preview" \
-    --bind "enter:become(docker exec -it {2} $argv)"
+    _docker_check; or return 1
+    _container_list | fzf --with-nth 2 \
+        --preview "fish $HOME/.config/fish/functions/docker/_fzf_preview.fish {1}" \
+        --bind "start:toggle-preview" \
+        --bind "enter:become(fish -c 'docker exec -it {2} $argv')"
 end
--- common-unix/.config/fish/functions/docker/dl.fish
+++ common-unix/.config/fish/functions/docker/dl.fish
@@ -1,10 +1,9 @@
 #!/usr/bin/env fish
 
 function dl --description "Logs of container"
-_docker_check
-or return 1
-_container_list -a | fzf --with-nth "2.." --query "$argv" \
-    --preview "fish $HOME/.config/fish/functions/docker/_fzf_preview.fish {1}" \
-    --bind "start:toggle-preview" \
-    --bind "enter:become(docker logs -f --since '$(date +%Y-%m-%dT%H:%M:%S)' {2})"
+    _docker_check; or return 1
+    _container_list -a | fzf --with-nth "2.." --query "$argv" \
+        --preview "fish $HOME/.config/fish/functions/docker/_fzf_preview.fish {1}" \
+        --bind "start:toggle-preview" \
+        --bind "enter:become(docker logs -f --since '$(date +%Y-%m-%dT%H:%M:%S)' {2})"
 end
