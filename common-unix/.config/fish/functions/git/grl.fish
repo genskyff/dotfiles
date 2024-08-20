02eb8f996d3b6349a100ffaf0a3b4896e9576464
Binwalker <gensky377@gmail.com>
2024-08-20 21:07:09 +0900

    chore: update git reflog date format to use two-digit year

--- common-unix/.config/fish/functions/git/grl.fish
+++ common-unix/.config/fish/functions/git/grl.fish
@@ -2,7 +2,7 @@
 
 function grl --description "git reflog with fzf"
     _git_check; or return 1
-    git reflog --color=always --date="format:%m/%d %H:%M" --format="%C(auto)%cd %h%d %gs" \
+    git reflog --color=always --date="format:%y/%m/%d %H:%M" --format="%C(auto)%cd %h%d %gs" \
         | fzf --preview "git show --color=always {3}" \
             --bind "enter:become(git checkout {3})"
 end
