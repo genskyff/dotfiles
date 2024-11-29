c421fa5a472688db1c7185d2af9c473db360a2c2
Kirisame <gensky377@gmail.com>
2024-11-30 00:39:42 +0900

    config(fish): remove default editor fallback in fe and ffe functions

--- common-unix/.config/fish/functions/ffe.fish
+++ common-unix/.config/fish/functions/ffe.fish
@@ -4,7 +4,6 @@ function ffe --description "Edit fish function"
     set function_path $HOME/.config/fish/functions
     set files $function_path/**/*.fish
     set nth (math (string split / -- $function_path | count) + 1)
-    test -n "$EDITOR"; or set EDITOR nvim
     string join \n $files \
         | fzf --with-nth "$nth.." --delimiter / --query "$argv" \
             --bind "start:toggle-preview" \
