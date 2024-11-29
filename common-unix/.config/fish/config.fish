dcc1f18e94c46bc6c3ac57981bee72f8cf9cbcc7
Kirisame <gensky377@gmail.com>
2024-11-30 00:39:14 +0900

    config(fish): streamline function path setup and update aliases

--- common-unix/.config/fish/config.fish
+++ common-unix/.config/fish/config.fish
@@ -2,42 +2,19 @@
 
 status is-interactive; or return
 
-fzf --fish | source
-starship init fish | source
-zoxide init fish | source
-
-set -gx EDITOR nvim
 set -gx fish_greeting
-
-set exclude ""
-test (uname) = "Darwin"
-and set exclude "-E Applications -E Library"
-set -gx FZF_CTRL_T_COMMAND "fd -tf -td -tl -L --color always $exclude"
-set -gx FZF_ALT_C_COMMAND "fd -td -L --color always $exclude"
-set -gx FZF_DEFAULT_COMMAND "fd -tf -td -tl -L --color always $exclude"
-set -gx FZF_DEFAULT_OPTS "--cycle --ansi --height 60% --highlight-line --reverse --info inline --border --no-separator \
-    --preview 'fish $HOME/.config/fish/functions/_fzf_preview.fish {}' \
-    --preview-window 'hidden,border-left,60%' \
-    --bind 'alt-/:change-preview-window(90%|60%)' \
-    --bind 'alt-,:toggle-wrap' \
-    --bind 'alt-.:toggle-preview-wrap' \
-    --bind 'ctrl-/:toggle-preview' \
-    --bind 'alt-f:preview-page-down,alt-b:preview-page-up'"
-
-set fish_function_path (path resolve $HOME/.config/fish/functions/**/) $fish_function_path
+set fish_function_path $HOME/.config/fish/functions/**/ $fish_function_path
 set fish_function_path (string join \n $fish_function_path | awk '!seen[$0]++')
 
 alias cls=clear
-alias cat=bat
 alias ff=fastfetch
-alias of=onefetch
-alias lg=lazygit
-alias lad=lazydocker
-alias zj=zellij
 alias bt="btm -b"
-
-alias vi=nvim
 alias vim=nvim
+alias cat=bat
+alias of=onefetch
+alias lad=lazydocker
+alias lg=lazygit
+alias zj=zellij
 
 alias ls="lsd -N"
 alias ll="ls -l"
@@ -51,4 +28,5 @@ alias gs="git status"
 alias gw="git switch"
 alias gd="git diff"
 alias gp="git pull"
+alias gss="git submodule status"
 alias gsu="git submodule update"
