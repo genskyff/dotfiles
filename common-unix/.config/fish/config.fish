7a8b18bf59bb4b18a8ad2fb9358748bd1b6c219e
Kirisame <gensky377@gmail.com>
2024-11-30 00:46:28 +0900

    config(fish): update function path setup to use realpath for improved accuracy

--- common-unix/.config/fish/config.fish
+++ common-unix/.config/fish/config.fish
@@ -3,7 +3,7 @@
 status is-interactive; or return
 
 set -gx fish_greeting
-set fish_function_path $HOME/.config/fish/functions/**/ $fish_function_path
+set fish_function_path (realpath $HOME/.config/fish/functions/**/) $fish_function_path
 set fish_function_path (string join \n $fish_function_path | awk '!seen[$0]++')
 
 alias cls=clear
