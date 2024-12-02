76dd2f9b457cb652eab691df1376c3486004c620
Binwalker <gensky377@gmail.com>
2024-12-02 23:46:46 +0900

    fix(fish): enhance fd command detection and streamline FZF command setup

--- common-unix/.config/fish/conf.d/fzf.fish
+++ common-unix/.config/fish/conf.d/fzf.fish
@@ -5,12 +5,20 @@ status is-interactive; or return
 command -q fzf; or return
 fzf --fish | source
 
-command -q fd; and set fd fd; or set fd fdfind
-test (uname) = "Darwin"; and set exclude "-E Applications -E Library"; or set exclude ""
+if command -q fd
+    set fd fd
+else if command -q fdfind
+    set fd fdfind
+end
 
-set -gx FZF_CTRL_T_COMMAND "$fd -tf -td -tl -L --color always $exclude"
-set -gx FZF_ALT_C_COMMAND "$fd -td -L --color always $exclude"
-set -gx FZF_DEFAULT_COMMAND "$fd -tf -td -tl -L --color always $exclude"
+if set -q fd
+    test (uname) = "Darwin"; and set exclude "-E Applications -E Library"; or set exclude ""
+
+    set -gx FZF_CTRL_T_COMMAND "$fd -tf -td -tl -L --color always $exclude"
+    set -gx FZF_ALT_C_COMMAND "$fd -td -L --color always $exclude"
+    set -gx FZF_DEFAULT_COMMAND "$fd -tf -td -tl -L --color always $exclude"
+end
+
 set -gx FZF_DEFAULT_OPTS "--cycle --ansi --height 60% --highlight-line --reverse --info inline --border --no-separator \
     --preview 'fish $HOME/.config/fish/functions/_fzf_preview.fish {}' \
     --preview-window 'hidden,border-left,60%' \
