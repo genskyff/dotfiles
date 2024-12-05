c6726c0f7d553533d9492b7ef7622af55b28c5be
Kirisame <gensky377@gmail.com>
2024-12-05 22:41:55 +0900

    fix(setup): remove unused package from AUR list

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ brew_list="bat bottom clang-format curl dust fastfetch fd fish fzf git-delta laz
 # Arch
 pacman_list="base-devel bat bind bottom clang curl docker docker-compose dust fastfetch fd fish fzf git git-delta lazygit less libunwind lsd neovim net-tools onefetch openbsd-netcat openssh ripgrep rustup socat starship sudo tokei traceroute unzip wget xmake zellij zoxide"
 yay_url=https://aur.archlinux.org/yay-bin.git
-aur_list="git-credential-oauth lazydocker-bin ttf-maple"
+aur_list="git-credential-oauth lazydocker-bin"
 
 # Debian
 apt_list="bat bind9-dnsutils build-essential clang-format clangd curl docker docker-compose fd-find fish git iptables less libunwind8 net-tools netcat-openbsd openssh-client openssh-server ripgrep socat sudo tmux traceroute unzip wget"
