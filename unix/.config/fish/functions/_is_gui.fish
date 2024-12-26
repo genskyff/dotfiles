78b0fdd3a20157a325ba2feeae17b008f50a8c08
Binwalker <gensky377@gmail.com>
2024-12-26 20:29:27 +0900

    feat(env): enhance editor selection with GUI check in fish configuration

---
+++ unix/.config/fish/functions/_is_gui.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+
+function _is_gui --description "Check shell environment"
+    if set -q SSH_CLIENT; or set -q SSH_CONNECTION; or set -q SSH_TTY
+        return 1
+    else if test (uname) = "Darwin"; and pgrep -q Finder
+        return 0
+    else if test (uname) = "Linux"; and set -q DISPLAY
+        return 0
+    else
+        return 1
+    end
+end
