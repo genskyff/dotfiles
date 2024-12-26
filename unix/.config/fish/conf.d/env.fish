9944c3de6437c38084309f6e7752f1b3276020f8
Kirisame <gensky377@gmail.com>
2024-12-26 18:34:45 +0900

    feat(env): add support for Helix editor in fish configuration

--- unix/.config/fish/conf.d/env.fish
+++ unix/.config/fish/conf.d/env.fish
@@ -3,6 +3,10 @@
 # EDITOR
 if command -q code
     set -gx EDITOR code
+else if command -q hx
+    set -gx EDITOR hx
+else if command -q helix
+    set -gx EDITOR helix
 else if command -q nvim
     set -gx EDITOR nvim
 end
