9e63071ea553bcc0e8f2a7d02da040f2ac8e70c3
Kirisame <gensky377@gmail.com>
2025-01-17 23:19:34 +0900

    fix(fish): set VISUAL editor to code when using GUI

--- unix/.config/fish/conf.d/env.fish
+++ unix/.config/fish/conf.d/env.fish
@@ -5,6 +5,7 @@ status is-interactive; or return 0
 # EDITOR
 if _is_gui; and command -q code
     set -gx EDITOR code
+    set -gx VISUAL code
 else if command -q hx
     set -gx EDITOR hx
 else if command -q helix
