729bbff70676b2d5d3b948b57d71229caa86171f
Kirisame <gensky377@gmail.com>
2025-01-17 21:42:24 +0900

    fix(fish): update file paths to use $__fish_config_dir for configuration functions

--- unix/.config/fish/functions/fe.fish
+++ unix/.config/fish/functions/fe.fish
@@ -1,9 +1,8 @@
 #!/usr/bin/env fish
 
 function fe --description "Edit fish configuration"
-    set config_path $HOME/.config/fish
-    set files $config_path/config.fish $config_path/conf.d/**/*.fish
-    set nth (math (string split / -- $config_path | count) + 1)
+    set files $__fish_config_dir/config.fish $__fish_config_dir/conf.d/**/*.fish
+    set nth (math (string split / -- $__fish_config_dir | count) + 1)
     string join \n $files \
         | fzf --with-nth "$nth.." --delimiter / --query "$argv" \
             --bind "start:toggle-preview" \
