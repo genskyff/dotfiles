58a9130e62774145ac3b199f1e8437eb9edf9066
Kirisame <gensky377@gmail.com>
2025-01-17 23:31:44 +0900

    fix(fish): update exclude patterns in fzf and rf functions for better compatibility on Darwin

--- unix/.config/fish/functions/rf.fish
+++ unix/.config/fish/functions/rf.fish
@@ -18,8 +18,8 @@ function rf --description "Find with ripgrep and fzf"
     end
 
     argparse h-hidden -- $argv; or return 1
-    test (uname) = Darwin; and set pattern "-g !Applications -g !Library"
-    set rg_prefix "rg -L --line-number --no-heading --color always --smart-case $_flag_hidden $pattern"
+    test (uname) = Darwin; and set exclude "-g !Applications -g !Library"
+    set rg_prefix "rg -L --line-number --no-heading --color always --smart-case $_flag_hidden $exclude"
     set toggle '
         if string match -q "*ripgrep*" "$FZF_PROMPT"
             echo "unbind(change)+change-prompt(fzf> )+enable-search+transform-query:echo \{q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f"
