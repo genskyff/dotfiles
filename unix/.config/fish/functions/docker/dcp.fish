22a5f1308d6f6f834f55df6a9fd4e74094916c93
Kirisame <gensky377@gmail.com>
2025-01-18 21:58:04 +0900

    fix(fish): enhance dcp function to support copying files between container and host with improved argument handling

--- unix/.config/fish/functions/docker/dcp.fish
+++ unix/.config/fish/functions/docker/dcp.fish
@@ -1,12 +1,20 @@
 #!/usr/bin/env fish
 
-function dcp --description "Copy files from container to host"
+function dcp --description "Copy files between container and host"
     _docker_check; or return 1
     _fzf_check; or return 1
 
-    test (count $argv) = 1; and set argv[2] (pwd)
+    argparse -N1 -X2 host -- $argv; or return 1
+    if set -q _flag_host
+        test (count $argv) = 1; and set argv[2] "\$(docker exec {2} bash -c 'echo \$HOME')"
+        set args "$argv[1] {2}:$argv[2]"
+    else
+        test (count $argv) = 1; and set argv[2] (pwd)
+        set args "{2}:$argv[1] $argv[2]"
+    end
+
     _container_list | fzf --with-nth 2 \
         --preview "fish $__fish_config_dir/functions/docker/_fzf_preview.fish {1}" \
         --bind "start:toggle-preview" \
-        --bind "enter:become(fish -c 'docker cp --follow-link {2}:$argv[1] $argv[2]')"
+        --bind "enter:become(fish -c \"docker cp --follow-link $args\")"
 end
