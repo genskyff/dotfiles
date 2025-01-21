cc99154dd524f3f6007308fefbf863b52f9d668d
Binwalker <gensky377@gmail.com>
2025-01-21 21:51:03 +0900

    fix(fish): update fzf commands to remove unnecessary flags

--- unix/.config/fish/functions/esc.fish
+++ unix/.config/fish/functions/esc.fish
@@ -7,7 +7,7 @@ function esc --description "Edit ssh configuration"
     set ssh_config_path $ssh_path/config
     set ssh_confd_path $ssh_path/conf.d
     test -f $ssh_config_path; and set files $ssh_config_path; or set files
-    test -d $ssh_confd_path; and set -a files (fd . $ssh_confd_path -Ltf)
+    test -d $ssh_confd_path; and set -a files (fd . $ssh_confd_path -tf -L)
     set nth (math (string split / -- $ssh_path | count) + 1)
     string join \n $files \
         | fzf --with-nth "$nth.." --delimiter / --query "$argv" \
