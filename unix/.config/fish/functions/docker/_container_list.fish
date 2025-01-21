6c1acdc3cc4cc49fef0cf5fb382dcfa8d3c54bbc
Binwalker <gensky377@gmail.com>
2025-01-21 22:25:04 +0900

    feat(fish): enhance _container_list function to accumulate exited and running containers

--- unix/.config/fish/functions/docker/_container_list.fish
+++ unix/.config/fish/functions/docker/_container_list.fish
@@ -3,11 +3,13 @@
 function _container_list --description "List containers"
     _docker_check; or return 1
 
+    set exited_containers
+    set running_containers
     docker ps --format "{{.ID}} {{.Names}} {{.Status}}" $argv | while read -l container_id container_name container_status
         if string match -qr "Exited*" $container_status
-            set exited_containers $exited_containers "$(set_color red)$container_id $container_name (Exited)$(set_color normal)"
+            set -a exited_containers "$(set_color red)$container_id $container_name (Exited)$(set_color normal)"
         else if string match -qr "Up*" $container_status
-            set running_containers $running_containers "$(set_color green)$container_id $container_name$(set_color normal)"
+            set -a running_containers "$(set_color green)$container_id $container_name$(set_color normal)"
         end
     end
 
