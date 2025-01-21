5bcaec32fda973db469500b3fddd74975cb6b2f7
Kirisame <gensky377@gmail.com>
2025-01-21 15:17:59 +0900

    fix(fish): update _fzf_preview function to use echo -e for improved formatting

--- unix/.config/fish/functions/docker/_fzf_preview.fish
+++ unix/.config/fish/functions/docker/_fzf_preview.fish
@@ -4,12 +4,12 @@ docker ps -a --filter id=$argv \
     --format "{{.ID}}|{{.Image}}|{{.Command}}|{{.CreatedAt}}|{{.Status}}|{{.Ports}}|{{.Names}}" \
     | while read -l line
         set -l fields (string split \| $line)
-        echo (set_color blue)"ID      "(set_color normal)$fields[1]
-        echo (set_color green)"Image   "(set_color normal)$fields[2]
-        echo (set_color yellow)"Command "(set_color normal)$fields[3]
-        echo (set_color cyan)"Created "(set_color normal)$fields[4]
-        echo (set_color yellow)"Status  "(set_color normal)$fields[5]
-        echo (set_color green)"Ports   "(set_color normal)$fields[6]
-        echo (set_color blue)"Name    "(set_color normal)$fields[7]
+        echo -e (set_color blue)"ID      "(set_color normal)$fields[1]
+        echo -e (set_color green)"Image   "(set_color normal)$fields[2]
+        echo -e (set_color yellow)"Command "(set_color normal)$fields[3]
+        echo -e (set_color cyan)"Created "(set_color normal)$fields[4]
+        echo -e (set_color yellow)"Status  "(set_color normal)$fields[5]
+        echo -e (set_color green)"Ports   "(set_color normal)$fields[6]
+        echo -e (set_color blue)"Name    "(set_color normal)$fields[7]
         echo ""
     end
