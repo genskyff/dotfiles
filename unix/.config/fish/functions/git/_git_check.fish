b999950250f0dee219c554fe7ca441bf3573637b
Kirisame <gensky377@gmail.com>
2025-01-21 16:17:41 +0900

    fix(fish): update error message handling in _docker_check and _git_check functions

--- unix/.config/fish/functions/git/_git_check.fish
+++ unix/.config/fish/functions/git/_git_check.fish
@@ -3,7 +3,7 @@
 function _git_check --description "Check git status"
    _cmd_check git; or return 1
 
-    set error_message (command git rev-parse --is-inside-work-tree 2>&1 1>/dev/null)
+    set error_message (command git rev-parse --is-inside-work-tree &>/dev/null)
     if test (string length -- "$error_message") -gt 0
         echo -e "$(set_color red)Error$(set_color normal): $error_message" >&2
         return 1
