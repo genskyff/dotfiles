6b0b2ee264836ef5c162008cb4b5c90aa2747094
Kirisame <gensky377@gmail.com>
2025-01-21 17:36:52 +0900

    fix(fish): update fe function to use glob pattern for loading configuration files

--- unix/.config/fish/functions/fe.fish
+++ unix/.config/fish/functions/fe.fish
@@ -1,13 +1,11 @@
 #!/usr/bin/env fish
 
 function fe --description "Edit fish configuration"
-    _cmd_check --fn fd; or return 1
     _cmd_check fzf; or return 1
 
     set fish_config_path $__fish_config_dir/config.fish
-    set fish_confd_path $__fish_config_dir/conf.d
     test -f $fish_config_path; and set files $fish_config_path; or set files
-    test -d $fish_confd_path; and set -a files (fd . $fish_confd_path -tf -L)
+    set -a files $__fish_config_dir/conf.d/**/*.fish
     set nth (math (string split / -- $__fish_config_dir | count) + 1)
     string join \n $files \
         | fzf --with-nth "$nth.." --delimiter / --query "$argv" \
