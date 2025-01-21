685a806d87e3b635588dc5070c879e2dfe303b5b
Kirisame <gensky377@gmail.com>
2025-01-21 14:39:18 +0900

    feat(fish): update efc function to use cmd_check and improve file handling

--- unix/.config/fish/functions/efc.fish
+++ unix/.config/fish/functions/efc.fish
@@ -1,9 +1,13 @@
 #!/usr/bin/env fish
 
 function efc --description "Edit fish configuration"
-    _fzf_check; or return 1
+    _cmd_check fd; or return 1
+    _cmd_check fzf; or return 1
 
-    set files $__fish_config_dir/config.fish $__fish_config_dir/conf.d/**/*.fish
+    set fish_config_path $__fish_config_dir/config.fish
+    set fish_confd_path $__fish_config_dir/conf.d
+    test -f $fish_config_path; and set files $fish_config_path; or set files
+    test -d $fish_confd_path; and set -a files (fd . $fish_confd_path -tf -L)
     set nth (math (string split / -- $__fish_config_dir | count) + 1)
     string join \n $files \
         | fzf --with-nth "$nth.." --delimiter / --query "$argv" \
