c95f7e38956c60784a3308d76c4e3d972b0f5bd6
Kirisame <gensky377@gmail.com>
2025-01-21 12:41:45 +0900

    feat(fish): add esc function for editing SSH configuration

---
+++ unix/.config/fish/functions/esc.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+
+function esc --description "Edit ssh configuration"
+    _fzf_check; or return 1
+
+    set ssh_path $HOME/.ssh
+    set ssh_config_path $ssh_path/config
+    set ssh_confd_path $ssh_path/conf.d
+    test -f $ssh_config_path; and set files $ssh_config_path; or set files
+    test -d $ssh_confd_path; and set -a files (fd . $ssh_confd_path -Ltf)
+    set nth (math (string split / -- $ssh_path | count) + 1)
+    string join \n $files \
+        | fzf --with-nth "$nth.." --delimiter / --query "$argv" \
+            --bind "start:toggle-preview" \
+            --bind "enter:become(fish -c '$EDITOR {}')"
+end
