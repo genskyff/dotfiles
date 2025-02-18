8909efbd1ba41dfd2ba3d70628d4373f95e6a02e
Binwalker <gensky377@gmail.com>
2025-02-18 22:42:10 +0900

    feat(setup): use linuxbrew instead nix

--- unix/.config/fish/conf.d/0brew.fish
+++ unix/.config/fish/conf.d/0brew.fish
@@ -1,2 +1,3 @@
-set -l brew_path /opt/homebrew/bin/brew
+test (uname) = Darwin; and set -l brew_path /opt/homebrew/bin/brew
+test (uname) = Linux; and set -l brew_path /home/linuxbrew/.linuxbrew/bin/brew
 test -f $brew_path; and eval "$($brew_path shellenv)"
