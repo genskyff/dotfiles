5e1bad62d86183ce81e65eceec33316f1d38fc1f
Binwalker <gensky377@gmail.com>
2025-03-02 18:06:35 +0900

    fix(profile): format FZF_DEFAULT_OPTS for improved readability

--- windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -34,13 +34,15 @@ if (Get-Command less -ErrorAction SilentlyContinue) {
 }
 
 if (Get-Command fzf -ErrorAction SilentlyContinue) {
-    $env:FZF_DEFAULT_OPTS = "--cycle --ansi --height 60% --highlight-line --reverse --info inline --border --no-separator
-                            --preview-window 'hidden,border-left,60%'
-                            --bind 'alt-/:change-preview-window(90%|60%)'
-                            --bind 'alt-,:toggle-wrap'
-                            --bind 'alt-.:toggle-preview-wrap'
-                            --bind 'ctrl-/:toggle-preview'
-                            --bind 'alt-f:preview-page-down,alt-b:preview-page-up'"
+    $env:FZF_DEFAULT_OPTS = "
+        --cycle --ansi --height 60% --highlight-line --reverse --info inline --border --no-separator
+        --preview-window 'hidden,border-left,60%'
+        --bind 'alt-/:change-preview-window(90%|60%)'
+        --bind 'alt-,:toggle-wrap'
+        --bind 'alt-.:toggle-preview-wrap'
+        --bind 'ctrl-/:toggle-preview'
+        --bind 'alt-f:preview-page-down,alt-b:preview-page-up'
+    "
 }
 
 function Open-Folder {
