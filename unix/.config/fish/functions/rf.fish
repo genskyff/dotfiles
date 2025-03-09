195700c128344e8d1f5d2e551cd1cae5e1fcec63
Binwalker <gensky377@gmail.com>
2025-03-09 15:52:07 +0900

    fix(rf): update temporary file names in ripgrep function

--- unix/.config/fish/functions/rf.fish
+++ unix/.config/fish/functions/rf.fish
@@ -14,9 +14,9 @@ function rf --description "Find with ripgrep and fzf"
     set rg_prefix "rg -L --line-number --no-heading --color always --smart-case $_flag_hidden $exclude"
     set toggle '
         if string match -q "*ripgrep*" "$FZF_PROMPT"
-            echo "unbind(change)+change-prompt(fzf> )+enable-search+transform-query:echo \{q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f"
+            echo "unbind(change)+change-prompt(fzf> )+enable-search+transform-query:echo \{q} > /tmp/rf-r; cat /tmp/rf-f"
         else
-            echo "rebind(change)+change-prompt(ripgrep> )+disable-search+transform-query:echo \{q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r"
+            echo "rebind(change)+change-prompt(ripgrep> )+disable-search+transform-query:echo \{q} > /tmp/rf-f; cat /tmp/rf-r"
         end'
 
     test "$EDITOR" = code; and set edit "$EDITOR -g {1}:{2}"; or set edit "$EDITOR {1} +{2}"
@@ -32,5 +32,5 @@ function rf --description "Find with ripgrep and fzf"
         --bind "change:reload:sleep 0.1; $rg_prefix {q} || true" \
         --bind "alt-0:transform:fish -c '$toggle'" \
         --bind "enter:become(fish -c '$edit')"
-    rm -f /tmp/rg-fzf-{r,f}
+    rm -f /tmp/rf-{r,f}
 end
