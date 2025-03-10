4c2254109520e48bef12931d07811c0902261d51
Binwalker <gensky377@gmail.com>
2025-03-10 21:00:56 +0900

    feat(ssh): add _ssh_config_list function to list SSH configuration files

---
+++ unix/.config/fish/functions/_ssh_config_list.fish
@@ -0,0 +1,20 @@
+function _ssh_config_list --description "List ssh configuration files"
+    if _cmd_check --quiet fd
+        set fd fd
+    else if _cmd_check --quiet fdfind
+        set fd fdfind
+    else
+        echo -e "$(set_color red)Error$(set_color normal): 'fd' command not found" >&2
+        return 1
+    end
+
+    set ssh_path $HOME/.ssh
+    set ssh_config_path $ssh_path/config
+    set ssh_confd_path $ssh_path/conf.d
+    test -f $ssh_config_path; and set list $ssh_config_path; or set list
+    test -d $ssh_confd_path; and set -a list ($fd . $ssh_confd_path -tf -L)
+
+    for file in $list
+        echo $file
+    end
+end
