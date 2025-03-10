a25aa9d385b8ee88a22b75cd4c7b36b52208d7a3
Kirisame <gensky377@gmail.com>
2025-03-10 12:21:47 +0900

    fix(fish): correct path handling in fe and ffe functions for accurate file listing

--- unix/.config/fish/functions/fe.fish
+++ unix/.config/fish/functions/fe.fish
@@ -4,7 +4,7 @@ function fe --description "Edit fish configuration"
     set fish_config_path $__fish_config_dir/config.fish
     test -f $fish_config_path; and set files $fish_config_path; or set files
     set -a files $__fish_config_dir/conf.d/**/*.fish
-    set nth (math (string split / -- $__fish_config_dir | count) + 1)
+    set nth (math (string split / $__fish_config_dir | count) + 1)
     string join \n $files \
         | fzf --with-nth "$nth.." -d/ \
             --bind "start:toggle-preview" \
--- unix/.config/fish/functions/ffe.fish
+++ unix/.config/fish/functions/ffe.fish
@@ -3,7 +3,7 @@ function ffe --description "Edit fish function"
 
     set function_path $__fish_config_dir/functions
     set files $function_path/**/*.fish
-    set nth (math (string split / -- $function_path | count) + 1)
+    set nth (math (string split / $function_path | count) + 1)
     string join \n $files \
         | fzf --with-nth "$nth.." -d/ \
             --bind "start:toggle-preview" \
