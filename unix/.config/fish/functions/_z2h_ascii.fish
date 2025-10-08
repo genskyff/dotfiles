8e78b08ee6b728825b078f4613816c95e7dfd7cf
Binwalker <gensky377@gmail.com>
2025-10-08 21:53:52 +0900

    feat(fish): enhance _z2h_ascii function to normalize full-width ASCII characters

--- unix/.config/fish/functions/_z2h_ascii.fish
+++ unix/.config/fish/functions/_z2h_ascii.fish
@@ -2,6 +2,25 @@ function _z2h_ascii --description 'Convert full-width ascii characters to half-w
     _cmd_check perl; or return 1
 
     set cmd (commandline)
-    set converted (printf "%s" "$cmd" | perl -CSD -Mutf8 -pe 'tr/\x{3000}\x{FF01}-\x{FF5E}/ \x21-\x7E/')
+    set converted (printf "%s" "$cmd" | perl -CS -Mutf8 -MUnicode::Normalize=NFKC -pe '
+        $_ = NFKC($_);
+
+        tr/\x{3000}\x{FF01}-\x{FF5E}/ \x21-\x7E/;
+
+        s/〜/~/g;
+        s/￥/\$/g;
+        s/【/[/g;
+        s/】/]/g;
+        s/「|『|」|』/\"/g;
+        s/——|—/_/g;
+        s/ー/-/g;
+        s/、/,/g;
+        s/‘|’/\'/g;
+        s/“|”/\"/g;
+        s/《/</g;
+        s/》/>/g;
+        s/。/./g;
+        s/・/\//g;
+    ')
     commandline --replace -- $converted
 end
