31527b75f11a7ce2a11737db1052672046bf1751
Binwalker <gensky377@gmail.com>
2025-10-08 22:39:25 +0900

    fix(fish): correct order of normalization and translation in _z2h_ascii function

--- unix/.config/fish/functions/_z2h_ascii.fish
+++ unix/.config/fish/functions/_z2h_ascii.fish
@@ -3,9 +3,9 @@ function _z2h_ascii --description 'Convert full-width ascii characters to half-w
 
     set cmd (commandline)
     set converted (printf "%s" "$cmd" | perl -CS -Mutf8 -MUnicode::Normalize=NFKC -pe '
-        $_ = NFKC($_);
+        s/……|…/^/g;
 
-        tr/\x{3000}\x{FF01}-\x{FF5E}/ \x21-\x7E/;
+        $_ = NFKC($_);
 
         s/〜/~/g;
         s/￥/\$/g;
@@ -21,6 +21,8 @@ function _z2h_ascii --description 'Convert full-width ascii characters to half-w
         s/》/>/g;
         s/。/./g;
         s/・/\//g;
+
+        tr/\x{3000}\x{FF01}-\x{FF5E}/ \x21-\x7E/;
     ')
     commandline --replace -- $converted
 end
