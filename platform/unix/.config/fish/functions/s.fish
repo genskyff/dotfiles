748398b7dee326ea1ec91db8eefbe0ff475b52ad
Binwalker <gensky377@gmail.com>
2026-09-17 23:10:57 +0900

    fix(ssh): handle case and indentation

--- platform/unix/.config/fish/functions/s.fish
+++ platform/unix/.config/fish/functions/s.fish
@@ -3,7 +3,7 @@ function s --description "SSH with fzf"
 
     set -l files (_ssh_config_list)
     set -l content (test -n "$files"; and cat $files)
-    set -l host (string join \n $content | grep -E '^\s*Host\s+\S+' | grep -v "*" | awk '{print $2}' | fzf --preview-window hidden)
+    set -l host (string join \n $content | grep -Ei '^\s*Host\s+\S+' | grep -v "*" | awk '{print $2}' | fzf --preview-window hidden)
     or return 1
 
     if command -q kitten; and set -q KITTY_PID; and string match -q xterm-kitty "$TERM"
