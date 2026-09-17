a92682f82dd70f1381f1268326482643c7595036
Binwalker <gensky377@gmail.com>
2026-09-18 01:00:46 +0900

    refactor(pwsh): share one color helper

--- lib/color.ps1
+++ lib/color.ps1
@@ -1,31 +1,8 @@
-function ok($msg, [switch]$n) {
-    if ($n) {
-        Write-Host $msg -ForegroundColor Green -NoNewline
-    } else {
-        Write-Host $msg -ForegroundColor Green
-    }
+function Write-Color($Color, $msg, [switch]$n) {
+    Write-Host $msg -ForegroundColor $Color -NoNewline:$n
 }
 
-function error($msg, [switch]$n) {
-    if ($n) {
-        Write-Host $msg -ForegroundColor Red -NoNewline
-    } else {
-        Write-Host $msg -ForegroundColor Red
-    }
-}
-
-function warn($msg, [switch]$n) {
-    if ($n) {
-        Write-Host $msg -ForegroundColor Yellow -NoNewline
-    } else {
-        Write-Host $msg -ForegroundColor Yellow
-    }
-}
-
-function info($msg, [switch]$n) {
-    if ($n) {
-        Write-Host $msg -ForegroundColor Blue -NoNewline
-    } else {
-        Write-Host $msg -ForegroundColor Blue
-    }
-}
+function ok { Write-Color Green @args }
+function error { Write-Color Red @args }
+function warn { Write-Color Yellow @args }
+function info { Write-Color Blue @args }
