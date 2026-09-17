559bb60b5e82d749d76d98b2cd57f4ce131e8776
Binwalker <gensky377@gmail.com>
2026-09-18 01:00:21 +0900

    refactor(fish): collect config files with path filter
    Replaces the manual test -f loops in _ssh_config_list and fe.

--- platform/unix/.config/fish/functions/_ssh_config_list.fish
+++ platform/unix/.config/fish/functions/_ssh_config_list.fish
@@ -1,15 +1,3 @@
 function _ssh_config_list --description "List ssh configuration files"
-    set -l ssh_dir $HOME/.ssh
-    set -l ssh_config_path $ssh_dir/config
-    set -l ssh_confd_dir $ssh_dir/conf.d
-    set -l files
-    test -f "$ssh_config_path"; and set -a files $ssh_config_path
-
-    for file in $ssh_confd_dir/**
-        test -f "$file"; and set -a files "$file"
-    end
-
-    for file in $files
-        echo "$file"
-    end
+    path filter -f $HOME/.ssh/config $HOME/.ssh/conf.d/**
 end
--- platform/unix/.config/fish/functions/fe.fish
+++ platform/unix/.config/fish/functions/fe.fish
@@ -1,9 +1,7 @@
 function fe --description "Edit fish configuration"
     _cmd_check fzf; or return 1
 
-    set -l fish_config_path $__fish_config_dir/config.fish
-    test -f $fish_config_path; and set -l files $fish_config_path; or set -l files
-    set -a files $__fish_config_dir/conf.d/**/*.fish
+    set -l files (path filter -f $__fish_config_dir/config.fish) $__fish_config_dir/conf.d/**/*.fish
     set -l nth (math (string split / "$__fish_config_dir" | count) + 1)
     string join \n $files \
         | fzf --with-nth "$nth.." -d/ \
